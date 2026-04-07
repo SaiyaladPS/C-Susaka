e2e9b908114fd00e00f8c7e3f8e379864326bce6
SaiyaladPS <jayj28222@gmail.com>
2026-04-07 20:07:07 +0700

    update

---
+++ Project/Project/frmConvertNumberToWord.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Project
+{
+    partial class frmConvertNumberToWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnConvertla = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.btnConverten = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Phetsarath OT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ປ້ອນຕົວເລກ";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(124, 85);
+            this.txtNumber.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(354, 26);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // btnConvertla
+            // 
+            this.btnConvertla.Font = new System.Drawing.Font("Phetsarath OT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertla.Location = new System.Drawing.Point(111, 157);
+            this.btnConvertla.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConvertla.Name = "btnConvertla";
+            this.btnConvertla.Size = new System.Drawing.Size(125, 35);
+            this.btnConvertla.TabIndex = 2;
+            this.btnConvertla.Text = "ອ່ານເປັນພາສາລາວ";
+            this.btnConvertla.UseVisualStyleBackColor = true;
+            this.btnConvertla.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Phetsarath OT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(366, 157);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 35);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "ລ້າງ";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Font = new System.Drawing.Font("Phetsarath OT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.Location = new System.Drawing.Point(56, 221);
+            this.lbResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(60, 23);
+            this.lbResult.TabIndex = 4;
+            this.lbResult.Text = "ຜົນທີ່ຮັບ";
+            // 
+            // btnConverten
+            // 
+            this.btnConverten.Font = new System.Drawing.Font("Phetsarath OT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConverten.Location = new System.Drawing.Point(244, 157);
+            this.btnConverten.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConverten.Name = "btnConverten";
+            this.btnConverten.Size = new System.Drawing.Size(112, 35);
+            this.btnConverten.TabIndex = 5;
+            this.btnConverten.Text = "ອ່ານເປັນອັງກິດ";
+            this.btnConverten.UseVisualStyleBackColor = true;
+            this.btnConverten.Click += new System.EventHandler(this.btnConverten_Click);
+            // 
+            // frmConvertNumberToWord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 363);
+            this.Controls.Add(this.btnConverten);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvertla);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmConvertNumberToWord";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnConvertla;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Button btnConverten;
+    }
+}
---
+++ Project/Project/frmConvertNumberToWord.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class frmConvertNumberToWord : Form
+    {
+        public frmConvertNumberToWord()
+        {
+            InitializeComponent();
+        }
+
+        string NumberToWord(int number)
+        {
+            if (number == 0)
+                return "ສູນ";
+
+            string[] Unit = { "", "ໜຶ່ງ", "ສອງ", "ສາມ", "ສີ່", "ຫ້າ", "ຫົກ", "ເຈັດ", "ແປດ", "ເກົ້າ" };
+            string[] Level = { "", "ສິບ", "ຮ້ອຍ", "ພັນ", "ໝື່ນ", "ແສນ", "ລ້ານ" };
+
+            string word = "";
+            int i = 0;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+
+                if (digit != 0)
+                {
+                    word = Unit[digit] + Level[i] + word;
+                }
+
+                number /= 10;
+                i++;
+            }
+
+            return word;
+        }
+
+        string NumberToWordEN(int number)
+        {
+            if (number == 0) return "Zero";
+
+            string[] units = {
+        "", "One", "Two", "Three", "Four", "Five",
+        "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+        "Twelve", "Thirteen", "Fourteen", "Fifteen",
+        "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    };
+
+            string[] tens = {
+        "", "", "Twenty", "Thirty", "Forty",
+        "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
+            if (number < 20)
+                return units[number];
+
+            if (number < 100)
+                return tens[number / 10] +
+                       ((number % 10 > 0) ? " " + units[number % 10] : "");
+
+            if (number < 1000)
+                return units[number / 100] + " Hundred" +
+                       ((number % 100 > 0) ? " " + NumberToWordEN(number % 100) : "");
+
+            if (number < 1000000)
+                return NumberToWordEN(number / 1000) + " Thousand" +
+                       ((number % 1000 > 0) ? " " + NumberToWordEN(number % 1000) : "");
+
+            if (number < 1000000000)
+                return NumberToWordEN(number / 1000000) + " Million" +
+                       ((number % 1000000 > 0) ? " " + NumberToWordEN(number % 1000000) : "");
+
+            return NumberToWordEN(number / 1000000000) + " Billion" +
+                   ((number % 1000000000 > 0) ? " " + NumberToWordEN(number % 1000000000) : "");
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            int number;
+
+            if (int.TryParse(txtNumber.Text, out number))
+            {
+                lbResult.Text = NumberToWord(number);
+            }
+            else
+            {
+                MessageBox.Show("ກະລຸນາປ້ອນຕົວເລກ");
+            }
+        }
+
+        private void btnConverten_Click(object sender, EventArgs e)
+        {
+            int number;
+
+            if (int.TryParse(txtNumber.Text, out number))
+            {
+                lbResult.Text = NumberToWordEN(number);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number");
+            }
+        }
+    }
+}
